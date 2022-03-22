e6725ef766cc4d76cf3745769581beb659cd67d5
Thalita Silva <100147594+ThalitaSolfacil@users.noreply.github.com>
2022-03-22 16:14:25 -0300

    Update testes_streamlit_cloud.py

--- testes_streamlit_cloud.py
+++ testes_streamlit_cloud.py
@@ -3,6 +3,9 @@ import pandas as pd
 import pyperclip3 as pc
 import clipboard as c
 import xerox
+import streamlit.components.v1 as components
+
+
 
 st.set_page_config(page_title = "Análise de IDs", page_icon=":mag:", layout = 'wide')
 
@@ -11,6 +14,22 @@ if st.button('Pandas'): pd.DataFrame(['Olá, pandas!']).to_clipboard(index =Fals
 if st.button('Pyperclip3'): pc.copy('Olá, Pyperclip3!')
 if st.button('Clipboard'): c.copy('Olá, clipboard!')
 if st.button('Xerox'): xerox.copy('Olá, xerox!')
+   
+
+html = """
+
+    <div>
+        <button onclick="copy()">Copy text</button>
+    </div>
     
+    <script>
+    function copy(){
+        navigator.clipboard.writeText('aaaaaaaaaaa');
+    }
+    </script>
+
+"""
+
+components.html(html)
     
     
